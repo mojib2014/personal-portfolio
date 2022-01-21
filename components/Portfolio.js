7688e3ed8d8f602c4053221281ed79ff449253bf
mojib2014 <mmohammad3@outlook.com>
2022-01-21 04:15:25 -0800

    Add projects (portfolio section)

--- components/Portfolio.js
+++ components/Portfolio.js
@@ -1,4 +1,3 @@
-import React from "react";
 import Image from "next/image";
 import coderscorner from "../public/portfolio/coderscorner.gif";
 import comfyhouse from "../public/portfolio/comfyhouse.png";
@@ -6,7 +5,9 @@ import crystalCollector from "../public/portfolio/crystalCollector.png";
 import dentsoft from "../public/portfolio/dent-soft.gif";
 import issueTracker from "../public/portfolio/issueTracker.png";
 import vidly from "../public/portfolio/vidly.png";
+import arrowRight from "../public/icons/arrow-right.svg";
 import styles from "../styles/Portfolio.module.css";
+import Link from "next/link";
 
 export default function Portfolio() {
   return (
@@ -15,16 +16,16 @@ export default function Portfolio() {
         <h1 className="font-bold text-4xl text-center xl:text-5xl mb-12">
           Portfolio
         </h1>
-        <div className={`${styles.flex} flex align-center justify-center`}>
-          <div className={`${styles.card} m-4 w-[33.3%] flex-[33.3%] card`}>
+        <div className={styles.grid}>
+          <div className={`${styles.card} m-4 card`}>
             <Image
               className="rounded"
               src={coderscorner}
               alt="Coders Corner Project"
-              width={300}
-              height={300}
+              width={350}
+              height={250}
             />
-            <div className="card-body">
+            <div className={styles.cardBody}>
               <h2 className="text-center font-bold text-2xl md:text-1xl my-3">
                 Coders Corner
               </h2>
@@ -32,34 +33,49 @@ export default function Portfolio() {
                 Asimple application for improving coding(software development)
                 Skills(for students and mentors).
               </p>
+              <Link href="/projects/coders-corner">
+                <a>Read more</a>
+              </Link>
             </div>
           </div>
-          <div className={`${styles.card} m-4 w-[33.3%] flex-[33.3%] card`}>
+          <div className={`${styles.card} m-4 card`}>
             <Image
               className="rounded"
               src={comfyhouse}
               alt="Comfy House Project"
-              width={300}
-              height={300}
+              width={350}
+              height={250}
             />
-            <div className="card-body">
+            <div className={styles.cardBody}>
               <h2 className="text-center font-bold text-2xl md:text-1xl my-3">
                 Comfy House
               </h2>
+              <p>A simple ecomerce shopping cart</p>
+              <Link href="/projects/comfy-house">
+                <a>Read more</a>
+              </Link>
             </div>
           </div>
-          <div className={`${styles.card} m-4 w-[33.3%] flex-[33.3%] card`}>
+          <div className={`${styles.card} m-4 card`}>
             <Image
               className="rounded"
               src={crystalCollector}
               alt="Crystal Collector Project"
-              width={300}
-              height={300}
+              width={350}
+              height={250}
             />
-            <div className="card-body">
+            <div className={styles.cardBody}>
               <h2 className="text-center font-bold text-2xl md:text-1xl my-3">
                 Crystal Collector
               </h2>
+              <p>
+                You will be given a random number at the start of the game.
+                There are four crystals below. By clicking on a crystal you will
+                add a specific amount of points to your total score.
+              </p>
+              <Link href="/projects/crystall-collector">
+                <a>Read more</a>
+              </Link>
             </div>
           </div>
         </div>
