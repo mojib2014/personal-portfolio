bd418fe1d474f49a430e80e0e42f12b2d13b7ef3
mojib2014 <mmohammad3@outlook.com>
2022-01-23 03:27:29 -0800

    Fix import statement in Header.js file

--- components/Header.js
+++ components/Header.js
@@ -2,7 +2,7 @@ import { useState, useEffect } from "react";
 import Link from "next/link";
 import "@fortawesome/fontawesome-free/css/all.css";
 import ActiveLink from "./common/ActiveLink";
-import styles from "../styles/header.module.css";
+import styles from "../styles/Header.module.css";
 
 export default function Header() {
   const [scrolled, setScrolled] = useState(false);
