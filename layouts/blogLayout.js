bd82939c41f188cd626f58be0614730fd100d178
mojib2014 <mmohammad3@outlook.com>
2022-01-28 15:48:35 -0800

    Add eslint-plugin-prettier and remove extra utils dir

--- layouts/blogLayout.js
+++ layouts/blogLayout.js
@@ -1,4 +1,3 @@
-import Link from "next/link";
 import Layout from "@/components/Layout";
 import SectionContainer from "@/components/SectionContainer";
 import Card from "@/components/common/Card";
@@ -13,25 +12,37 @@ export default function BlogLayout({
   onChange,
   onSubmit,
 }) {
+  const filteredBlogPosts = posts.filter((frontMatter) => {
+    const searchContent =
+      frontMatter.title + frontMatter.summary + frontMatter.tags.join(" ");
+    return searchContent.toLowerCase().includes(query.toLowerCase());
+  });
+
+  // If initialDisplayPosts exist, display it if no searchValue is specified
+  const displayPosts =
+    initialDisplayPosts.length > 0 && !query
+      ? initialDisplayPosts
+      : filteredBlogPosts;
+
   return (
     <Layout>
-      <article className="divide-y pt-90">
-        <SectionContainer>
+      <SectionContainer>
+        <article className="divide-y pt-90 mt-90">
           <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
             All Posts
           </h1>
-          <div className="space-x-2 md:space-x-5">
-            <div className="max-w-">
-              {posts.map((post) => (
+          <div className="space-x-2 md:space-x-5 space-y-2 md:space-y-5 lg:grid lg:grid-cols-4 lg:space-y-0 lg:items-baseline">
+            <div className="space-y-3 xl:col-span-3">
+              {displayPosts.map((post) => (
                 <Card key={post.title} item={post} />
               ))}
             </div>
-            <div className="max-w-md flex flex-col items-center border border-gray p-4">
+            <dl className="flex flex-col items-center border border-gray p-4">
               <Search value={query} onChange={onChange} onSubmit={onSubmit} />
-            </div>
+            </dl>
           </div>
-        </SectionContainer>
-      </article>
+        </article>
+      </SectionContainer>
     </Layout>
   );
 }
